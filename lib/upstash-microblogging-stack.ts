6c9a6e7d640ada3875b279d1b4c047a784d4d66f
Kay Plößer <k@kay.is>
2021-05-11 15:18:53 +0000

    Integrate Lambda with Upstash

--- lib/upstash-microblogging-stack.ts
+++ lib/upstash-microblogging-stack.ts
@@ -1,10 +1,24 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as path from "path";
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class UpstashMicrobloggingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new apigateway.LambdaRestApi(this, "MyApi", {
+      handler: new lambda.Function(this, "MyFunction", {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "index.handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, "backend")),
+        environment: {
+          REDIS_ENDPOINT: "<DATABASE_ENDPOINT>",
+          REDIS_PORT: "<DATABASE_PORT>",
+          REDIS_PASSWORD: "<DATABASE_PASSWORD>",
+        },
+      }),
+    });
   }
 }
